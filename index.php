fbc17e2b3fde54c0a0a5bc3d81a9d6035c96d07e
tonoy58 <34272188+tonoy58@users.noreply.github.com>
2017-12-12 23:32:56 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-<?php include 'https://tonoy58.github.io/header.php' ?>
+<html>
+	
+	<body>
+	<?php include 'https://tonoy58.github.io/header.php' ?>
 <div class="content">
 	<h3 style="font-size: 20px;">Welcome to my personal profile. I make this profile to my own ability. I think it will be the perfect profile at first time.</h3>
 </div>
 <?php include 'https://tonoy58.github.io/footer.php' ?>
+	</body>
+	</html>
